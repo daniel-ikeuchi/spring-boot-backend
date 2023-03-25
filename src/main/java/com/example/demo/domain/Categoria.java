6313e62e07d9a022cf2b7e4eaaaa0ca32321dbe2
Daniel K I Ikeuchi <daniel.ikeuchi@gmail.com>
2023-03-25 09:39:50 -0300

    Banco de dados H2 e criacao automatica da tabela - Categoria

--- src/main/java/com/example/demo/domain/Categoria.java
+++ src/main/java/com/example/demo/domain/Categoria.java
@@ -2,20 +2,28 @@ package com.example.demo.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String name;
+	private String nome;
 	
 	public Categoria() {
 	}
 	
-	public Categoria(Integer id, String name) {
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.nome = nome;
 	}
 
 	public Integer getId() {
@@ -26,12 +34,12 @@ public class Categoria implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	@Override
