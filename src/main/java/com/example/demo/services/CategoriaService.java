ccee81ead2c7546b58ed0a5226dd6e46096aaf2d
Daniel K I Ikeuchi <daniel.ikeuchi@gmail.com>
2023-04-04 15:19:43 -0300

    Deletando uma Categoria via DELETE

--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -3,32 +3,43 @@ package com.example.demo.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Categoria;
-import com.example.demo.exceptions.ObjectNotFoundException;
 import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.services.exceptions.DataIntegrityException;
+import com.example.demo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
-	public Categoria findById(Integer id) {
-		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado => id: " + id + " | Tipo: " + Categoria.class.getName()));
-	}
 
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
 
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado => id: " + id + " | Tipo: " + Categoria.class.getName()));
+	}
+
 	public Categoria update(Categoria obj) {
 		findById(obj.getId());
 		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
 	}
 	
 }
