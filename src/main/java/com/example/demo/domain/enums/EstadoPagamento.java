50a100d050ac0c166ecdc924e8a607106b0aa5b0
daniel-ikeuchi <daniel.ikeuchi@gmail.com>
2023-06-20 15:47:22 -0300

    Implementando toString de Pedido

--- src/main/java/com/example/demo/domain/enums/EstadoPagamento.java
+++ src/main/java/com/example/demo/domain/enums/EstadoPagamento.java
@@ -2,7 +2,7 @@ package com.example.demo.domain.enums;
 
 public enum EstadoPagamento {
 
-	PENDENTE(1, "Pendendte"),
+	PENDENTE(1, "Pendente"),
 	QUITADO(2, "Quitado"),
 	CANCELADO(3, "Cancelado");
 	
